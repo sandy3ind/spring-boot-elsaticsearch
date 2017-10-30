9dfa8eae7548ac21843a83a38e2720aaa0a1c84f
sandeep sharma <sandeep.sharma@trantorinc.com>
2017-10-30 17:44:38 +0530

    Initial Commits

---
+++ src/main/java/com/javagain/springboot/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.javagain.springboot.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.javagain.springboot.document.Customer;
+
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+	
+	public Customer findByFirstName(String firstName);
+	
+    public List<Customer> findByLastName(String lastName);
+    
+}
